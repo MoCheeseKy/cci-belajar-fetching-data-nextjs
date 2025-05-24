4e5a602efa4cf8cbd60d3e0f505f416c059a7659
Your NameMoCheeseKy <rifkyking04@gmail.com>
2025-05-24 19:15:10 +0700

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,3 @@
-import axios from 'axios';
-
-// GET
-function fetch() {
-  axios
-    .get('/api/hello')
-    .then((ress) => {
-      console.log(ress);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
 export default function Home() {
   return (
     <main>
